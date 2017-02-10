c5dcc8a50522cb979c3c3ef6fdb6431b522811f1
disapand <shadow724156286@hotmail.com>
2017-02-10 11:39:45 +0800

    finished 80%

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         // lockAnchors: true,                                           //锁定锚点
         // slidesNavigation: true,                                      //横向的左右滑动导航(false)
         // slidesNavPosition: 'bottom',                                 //slideNavigation的位置(top, bottom)
-
+        controlArrowColor: '#333', //箭头的颜色
         //Scrolling
         css3: false, //是否使用css3滑动效果(true)
         scrollingSpeed: 1200, //滑动速度(700)
@@ -40,6 +40,37 @@ $(document).ready(function () {
 
     $('#photo').mouseleave(function (event) {
         $(this).addClass('animated pulse-only-min').removeClass('pulse-only-max');
+    });
+
+    var linkList = $('.item-link'),
+        hoverList = $('.item-hover');
+
+    for (var i = 0; i < hoverList.length; i++) {
+        linkList.eq(i).on('mouseenter', { key: i }, function (event) {
+            $(this).attr('style', 'opacity: 0;').removeClass('animated flipInY');
+            hoverList.eq(event.data.key).addClass('animated flipInY').removeAttr('style');
+        }).on('mouseleave', { key: i }, function (event) {
+            $(this).removeAttr('style').addClass('animated flipInY');
+            hoverList.eq(event.data.key).removeClass('animated flipInY').attr('style', 'opacity: 0;');
+        });
+    }
+
+    linkList.eq(8).click(function (event) {
+        $('#intro-group2').animate({ 'left': '20%' }, 1000);
+        $('#intro-group1').animate({ 'margin-left': '-100%' }, 1000);
+    });
+
+    linkList.eq(14).click(function (event) {
+        $('#intro-group2').animate({ 'left': '100%' }, 1000);
+        $('#intro-group1').animate({ 'margin-left': '20%' }, 1000);
+    });
+
+    $('#preview').click(function (event) {
+        $.fn.fullpage.moveTo(3);
+    });
+
+    $('#shop').click(function (event) {
+        window.open("https://shop117316021.taobao.com/", "_blank");
     });
 });
 //# sourceMappingURL=main.js.map
